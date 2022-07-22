51ad5f4da21637029c145795d6a4b02e84de477f
Fayik <veznedaroglufayik2@gmail.com>
2022-07-22 15:42:52 +0300

    22.07.2022/15:42

--- BInaryTree/BinaryTree.cs
+++ BInaryTree/BinaryTree.cs
@@ -6,40 +6,40 @@ using System.Threading.Tasks;
 
 namespace BInaryTree
 {
-    public class BinaryTree<T> where T:IComparable
+    public class BinaryTree<T> where T : IComparable
     {
         //Şimdi recursive bir çağrı yapalım
         public List<Node<T>> list { get; private set; }
         public BinaryTree()
         {
-            list= new List<Node<T>>();  
+            list = new List<Node<T>>();
         }
         public List<Node<T>> InOrder(Node<T> root)
         {
-            if (!(root==null))//root ifadesi null olmadığı sürece dolaşmamız gerekmektedir
+            if (!(root == null))//root ifadesi null olmadığı sürece dolaşmamız gerekmektedir
             {
                 InOrder(root.Left);
                 list.Add(root);
-                InOrder(root.Right);    
+                InOrder(root.Right);
             }
             return list;
         }
         public List<Node<T>> InOrderNonRecursiveTraversal(Node<T> root)
         {
-            var list=new List<Node<T>>();
+            var list = new List<Node<T>>();
             var S = new Stack<Node<T>>();
             Node<T> currentNode = root;
             bool done = false;
             while (!done)
             {
-                if (currentNode!=null)
+                if (currentNode != null)
                 {
                     S.Push(currentNode);
                     currentNode = currentNode.Left;
                 }
                 else
                 {
-                    if (S.Count==0)
+                    if (S.Count == 0)
                     {
 
                         done = true;
@@ -48,7 +48,7 @@ namespace BInaryTree
                     {
                         currentNode = S.Pop();
                         list.Add(currentNode);
-                        currentNode=currentNode.Right;
+                        currentNode = currentNode.Right;
                     }
                 }
             }
@@ -56,7 +56,7 @@ namespace BInaryTree
         }
         public List<Node<T>> PreOrder(Node<T> root)
         {
-            if (!(root==null))
+            if (!(root == null))
             {
                 list.Add(root);
                 PreOrder(root.Left);
@@ -66,23 +66,23 @@ namespace BInaryTree
         }
         public List<Node<T>> PreOrderNonRecursiveTraversal(Node<T> root)
         {
-            var list= new List<Node<T>>();//liste yapımızı oluşturduk.
+            var list = new List<Node<T>>();//liste yapımızı oluşturduk.
             var s = new Stack<Node<T>>();//stack(yığın) yapımızı oluşturduk.
-            if (root==null)
+            if (root == null)
             {
                 throw new ArgumentNullException("root");
             }
             s.Push(root);
-            while (!(s.Count==0))
+            while (!(s.Count == 0))
             {
                 var temp = s.Pop();
                 list.Add(temp);
-                if (temp.Right!=null)
+                if (temp.Right != null)
                 {
                     s.Push(temp.Right);
 
                 }
-                if (temp.Left!=null)
+                if (temp.Left != null)
                 {
                     s.Push(temp.Left);
                 }
@@ -91,9 +91,9 @@ namespace BInaryTree
         }
         public List<Node<T>> PostOrder(Node<T> root)
         {
-            if (!(root==null))
+            if (!(root == null))
             {
-                
+
                 PostOrder(root.Left);
                 PostOrder(root.Right);
                 list.Add(root);
@@ -107,16 +107,16 @@ namespace BInaryTree
             var list = new List<Node<T>>();
             var Q = new Queue<Node<T>>();
             Q.Enqueue(root);//parametre olarak aldığım root ifadesini Enqueue ile " kuyruğuna eklmesini yapıyorum.
-            while (Q.Count>0)
+            while (Q.Count > 0)
             {
-                var temp=Q.Dequeue();//Koşul devam ettiği sürece kuyruktan bir elemanı aıp temp değişkenine atıyorum.
+                var temp = Q.Dequeue();//Koşul devam ettiği sürece kuyruktan bir elemanı aıp temp değişkenine atıyorum.
                 list.Add(temp);
                 if (temp.Left != null)
                 {
                     Q.Enqueue(temp.Left);
                 }
-                
-                if (temp.Right!=null)
+
+                if (temp.Right != null)
                 {
                     Q.Enqueue(temp.Right);
 
@@ -124,5 +124,168 @@ namespace BInaryTree
             }
             return list;
         }
+
+        public Node<T> FindMax(Node<T> root)//Binary tree'de maksimum kökü bulalım.
+        {
+            var current = root;
+            while (!(current.Right == null))//current null'dan farklı bir duruma sahip ise döngüden çıkmamız beklenecektir.
+            {
+                current = current.Right;
+
+            }
+            return current;
+        }
+
+        public Node<T> FindMin(Node<T> root)
+        {
+            var current = root;
+            while (!(current.Left==null))
+            {
+                current=current.Left;
+            }
+            return current;
+        }
+       
+        //Binary tree'de istenen değeri bulmaya yarayan metod yazmaktayız.Ve bu metod yine verilen düz diziyi sıralama işlemini yaparkenki
+        // gibi kök'ten başlayıp büyüklük küçüklük ilişkisine göre,küçük ise sol'dan,büyük ise sağ taraftan ilerleme göstermektedir.
+        public Node<T> Find(Node<T> root,T key)
+        {
+            //key aranan değerdir
+            var current = root;
+            while (key.CompareTo(current.Value)!=0)
+            {
+                if (key.CompareTo(current.Value)<0)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+
+                }
+                if (current == null)
+                {
+                    throw new Exception("Could not found!");
+                }
+            }
+            return current;
+        }
+        public Node<T> Remove(Node<T> root,T key)
+        {
+            if (root == null)
+            {
+                return root;//throw new ArgumentNullException();
+            }
+            if (key.CompareTo(root.Value)<0)
+            {
+                root.Left = Remove(root.Left, key);
+            }
+            else if(key.CompareTo(root.Value) > 0)
+            {
+                root.Right=Remove(root.Right, key); 
+            }
+            else
+            {
+                //silme işlemini uygulayabiliriz.
+                //tek çocuk yada çocuk yok ise.
+                if (root.Left == null)
+                {
+                    return root.Right;
+                }
+                else if (root.Right == null)
+                {
+                    return root.Left;
+                }
+                //iki çocuk var ise
+                root.Value = FindMax(root.Right).Value;
+                root.Right= Remove(root.Right, key);
+            }
+            return root;
+
+        }
+        public static int MaxDepth(Node<T> root)
+        {
+            if (root==null)
+            {
+                return 0;
+            }
+            int LeftDepth=MaxDepth(root.Left);
+            int RightDepth=MaxDepth(root.Right);
+            return (LeftDepth > RightDepth) ?
+                LeftDepth + 1:
+                RightDepth + 1;
+
+        }
+        public Node<T> DeepestNode(Node<T> root)
+        {
+            Node<T> temp = null;
+            if (root==null)
+            {
+                throw new Exception("Empty tree");
+                var q = new Queue<Node<T>>();
+                q.Enqueue(root);
+                while (q.Count>0)
+                {
+                    temp = q.Dequeue();
+                    if (temp.Left != null)
+                    {
+                        q.Enqueue(temp.Left);
+
+                    }
+                    if (temp.Right != null)
+                    {
+                        q.Enqueue(temp.Right);
+                    }
+                    
+                }
+
+            }
+            return temp;
+        }
+       //Yaprakları saymak için kullanılır.
+        public int NumberOfLeafs(Node<T> root)
+        {
+            int count = 0;
+            if (root==null)
+            {
+                return count;
+            }
+            var q =new Queue<Node<T>>();
+            q.Enqueue(root);
+            while (q.Count>0)
+            {
+
+                var temp = q.Dequeue();
+                if (temp.Left == null && temp.Right == null)
+                    count++;
+                if (temp.Left!=null)
+                {
+                    q.Enqueue(temp.Left);
+                }
+                if (temp.Right!=null)
+                {
+                    q.Enqueue(temp.Left);
+                }
+
+            }
+            return count;
+        }
+        public void PrintPaths(Node<T> root)
+        {
+            var path = new T[256];//yolların organizasyonu için path şeklinde bir tanımlma yapacağız.
+            PrintPaths(root,path, 0);
+        
+        }
+        private void PrintArray(T[] path,int len)
+        {
+            for(int i = 0; i < len; i++)
+            {
+                Console.Write($"{path[i]}");
+            }
+            Console.WriteLine();
+        }
+
     }
-}
+
+    }
+
--- B_S_TREE/Program.cs
+++ B_S_TREE/Program.cs
@@ -11,23 +11,41 @@ namespace B_S_TREE
     {
         static void Main(string[] args)
         {
-            var BST = new BST<int>(new int[] { 23, 16, 45, 3, 22, 37, 99 });
-
-            //new BinaryTree<int>().InOrder(BST.Root).ForEach(node =>Console.Write(node+ " "));//InOrder sıralam işlemi gösterilmiştir.
-            //new BinaryTree<int>().InOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));//non recursive ifade ile (iteratif çalışan) InOrder ifadeyi yazdırma işlemini gerçekleştirdik.
-
-            //new BinaryTree<int>().PreOrder(BST.Root).ForEach(node =>Console.Write(node+ " "));//Yukarıda girilen elemanların ikili ağaç sisteminde preorder sıralama process'i gösterilmiştir.
-            //new BinaryTree<int>().PreOrder(BST.Root).ForEach(node => Console.Write(node + " "));//Yukarıda girilen elemanların ikili ağaç sisteminde preorder sağ sıralama process'i gösterilmiştir.
-            //new BinaryTree<int>().PreOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));
-
-            //Console.WriteLine();
-            //new BinaryTree<int>().PostOrder(BST.Root).ForEach(node => Console.Write(node + " "));//PostOrder ikili arama mantığıyla sıralama işlemini gerçekleştirdik.
-            //Console.WriteLine();
-            //new BinaryTree<int>().LevelOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));
+            var BST = new BST<int>(new int[] { 60, 40, 70, 20, 45, 65, 85 });
 
 
+            {
+                //new BinaryTree<int>().InOrder(BST.Root).ForEach(node => Console.Write(node + " "));
+                //Console.WriteLine($"{new BinaryTree<int>().Remove(BST.Root, 20)}");
+                //new BinaryTree<int>().InOrder(BST.Root).ForEach(node => Console.Write(node + " "));//Kaldırma işleminden sonra 20 değerinin olmadığını göreceğiz.
 
-            Console.ReadKey();
+                
+
+
+                //new BinaryTree<int>().InOrder(BST.Root).ForEach(node =>Console.Write(node+ " "));//InOrder sıralam işlemi gösterilmiştir.
+                //new BinaryTree<int>().InOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));//non recursive ifade ile (iteratif çalışan) InOrder ifadeyi yazdırma işlemini gerçekleştirdik.
+
+                //new BinaryTree<int>().PreOrder(BST.Root).ForEach(node =>Console.Write(node+ " "));//Yukarıda girilen elemanların ikili ağaç sisteminde preorder sıralama process'i gösterilmiştir.
+                //new BinaryTree<int>().PreOrder(BST.Root).ForEach(node => Console.Write(node + " "));//Yukarıda girilen elemanların ikili ağaç sisteminde preorder sağ sıralama process'i gösterilmiştir.
+                //new BinaryTree<int>().PreOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));
+
+                //Console.WriteLine();
+                //new BinaryTree<int>().PostOrder(BST.Root).ForEach(node => Console.Write(node + " "));//PostOrder ikili arama mantığıyla sıralama işlemini gerçekleştirdik.
+                //Console.WriteLine();
+                //new BinaryTree<int>().LevelOrderNonRecursiveTraversal(BST.Root).ForEach(node => Console.Write(node + " "));
+
+                //Binary Tree'de köklerden maksimum olanı bulmaya çalışıyoruz.
+                //Console.WriteLine($"Maximum Value: {new BinaryTree<int>().FindMax(BST.Root)}");
+                ////Bianry Tree'de köklerden minimum olanı bulmaya çalışıyoruz.
+                //Console.WriteLine($"Maximum Value: {new BinaryTree<int>().FindMin(BST.Root)}");
+                ////ikili arama ağacında bulma işlemi.
+                //Console.WriteLine($"{new BinaryTree<int>().Find(BST.Root, 20)}");
+
+
+
+                Console.ReadKey();
+            }
         }
+
     }
 }
